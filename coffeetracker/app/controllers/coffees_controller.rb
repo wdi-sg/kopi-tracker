a202e4c6579abe67ba2abcb332c57a73e44a968e
andrealmj <andrealmj@live.com>
2019-01-29 16:17:17 +0800

    added controllers

---
+++ coffeetracker/app/controllers/coffees_controller.rb
@@ -0,0 +1,27 @@
+class CoffeesController < ApplicationController
+    def new
+        @origins = Origin.all
+    end
+
+    def create
+        @coffee = Coffee.new(coffee_params)
+
+        @coffee.save
+        redirect_to @coffee
+    end
+
+    def show
+        # deal with the case that we are trying to get a coffee for a farm that doesn't exist
+
+        @coffee = Coffee.find(params[:id])
+
+        if params[:origin_id].to_i != @coffee.origin.id
+        # do something
+        end
+    end
+
+    private
+    def coffee_params
+        params.require(:coffee).permit(:name, :origin_id)
+    end
+end
