29319bfce924929fcefb2cc55627022f0efd9148
khyreerusydi <nicoraventbh@gmail.com>
2019-04-29 20:06:34 +0800

    created seed file

--- GoldGroundRoastery/db/seeds.rb
+++ GoldGroundRoastery/db/seeds.rb
@@ -3,5 +3,17 @@
 #
 # Examples:
 #
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+  # movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
+  # Character.create(name: 'Luke', movie: movies.first)
+
+Roast.create(roast: "Cinnamon")
+Roast.create(roast: "Light")
+Roast.create(roast: "City/Medium")
+Roast.create(roast: "Full City")
+Roast.create(roast: "Dark")
+Roast.create(roast: "French")
+Roast.create(roast: "Italian")
+
+Origin.create(location: "Indonesia", phone: "119-297-6442")
+Origin.create(location: "Columbia", phone: "998-666-8624")
+Origin.create(location: "India", phone: "99885263")
