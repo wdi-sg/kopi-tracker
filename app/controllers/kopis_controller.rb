5ff3502472d8b7b5b8aa6b3b73f22260202f15c4
claucanchin <claucanchin@gmail.com>
2019-05-02 01:55:37 +0800

    added devise

--- app/controllers/kopis_controller.rb
+++ app/controllers/kopis_controller.rb
@@ -1,4 +1,7 @@
 class KopisController < ApplicationController
+
+before_action :authenticate_user!, :except => [ :show, :index ]
+
   def index
     @kopis = Kopi.all.order(params[:sort])
     @customers = Customer.all
