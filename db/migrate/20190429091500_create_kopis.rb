9f9133ceb61575bbff33e3074f7c27d58c3bb64a
puppypaws <marcusjoshualee@gmail.com>
2019-05-02 11:00:51 +0800

    deliverables but data cant be displayed yet

---
+++ db/migrate/20190429091500_create_kopis.rb
@@ -0,0 +1,10 @@
+class CreateKopis < ActiveRecord::Migration[5.2]
+  def change
+    create_table :kopis do |t|
+      t.string :name
+      t.string :roast
+
+      t.timestamps
+    end
+  end
+end
