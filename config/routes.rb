f7f9a906478c4bb90b1863818757b896b51c5ae0
seeo <aragonlotr5@gmail.com>
2019-05-03 11:02:00 +0800

    commiting stuff up to swee chin's kopi-tracker repo, with a gitignore file that ignores config folder and database.yml

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'kopis#index'
 
+
   # routing for kopis #
   get '/kopis/new' => 'kopis#new', as: 'new_kopi'
   get '/kopis/:id' => 'kopis#show' , as: 'kopi'
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,7 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-Roast.destroy_all
+Roast.destroy_all  #TOASK: what does this do?
 
 Roast.create(name: "Cinnamon")
 Roast.create(name: "Light")
@@ -18,7 +18,7 @@ Roast.create(name: "Italian")
 p "SEEDED ROAST DATA"
 
 ##################################
-Origin.destroy_all
+Origin.destroy_all  #TOASK: what does this do?
 
 Origin.create(name: "Farmland", location: "Singapore", phone: "9092 0123")
 Origin.create(name: "Overgrowth Farm", location: "USA", phone: "9810 6692 0123")
