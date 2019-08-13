a31287e7c7a005d959b69c1d35a7f43a5aa633ae
EliseChia <swedesurrender@gmail.com>
2019-08-13 22:47:01 +0800

    done table for kopis, origins, roasts. seeded origins and roasts

---
+++ kopi-tracker/db/migrate/20190813141133_create_kopis.rb
@@ -0,0 +1,9 @@
+class CreateKopis < ActiveRecord::Migration[5.2]
+  def change
+    create_table :kopis do |t|
+      t.string :name
+      t.references :roast
+      t.references :origin
+    end
+  end
+end
