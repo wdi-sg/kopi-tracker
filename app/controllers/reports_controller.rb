c3f806593ac9c1f1f59ae2551bb7e0880e9a050f
kach92 <kenny.ang9206@gmail.com>
2019-08-13 22:59:52 +0800

    dono how to do display by kopi or origins

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -1,7 +1,9 @@
 class ReportsController < ApplicationController
     def index
-        @orders = Order.all
-        @filtered_orders = @orders.select{|x|x.created_at.in_time_zone('Singapore')>Time.now.strftime("%d")}
+
+            @orders = Order.all
+            @filtered_orders = @orders.select{|x|x.created_at.in_time_zone('Singapore')>Time.now.strftime("%d")}
+
 
     end
 end
