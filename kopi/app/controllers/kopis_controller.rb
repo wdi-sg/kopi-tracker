c991354f3c28484a06ae98766a6343356a9ff1d2
aliciawongg <w.alicia@gmail.com>
2019-08-13 18:48:25 +0800

    first commit

---
+++ kopi/app/controllers/kopis_controller.rb
@@ -0,0 +1,25 @@
+class KopisController < ApplicationController
+
+  def new
+
+  end
+
+  def create
+    @kopi = Kopi.new(kopi_params)
+
+    @kopi.save
+    redirect_to @kopi
+  end
+
+  def show
+    @kopi = Kopi.find(params[:id])
+  end
+
+private
+
+  def kopi_params
+    params.require(:kopi).permit(:name, :roast_id, :origin_id)
+
+  end
+
+end
