4ecb0cd3747f884519a420d0bba04c899aa46bb1
irockmysock <benny_chin89@hotmail.com>
2019-08-14 16:04:00 +0800

    added sales by kopi in report route

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -3,6 +3,11 @@ class ReportsController < ApplicationController
   def index
     @orders = Order.all
 
+    @kopiGroups = Order.all.group(:kopi_id).select("kopi_id,sum(weight) as weight")
+    @kopiGroups.each do |group|
+      p group
+    end
+
   end
 
 end
