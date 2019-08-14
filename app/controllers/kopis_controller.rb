68459ce3cebbe615afc7e0f0b800f3b14a6585f3
sowyuen <sowyuenl@gmail.com>
2019-08-14 09:34:28 +0800

    incomplete..

---
+++ app/controllers/kopis_controller.rb
@@ -0,0 +1,29 @@
+class KopisController < ApplicationController
+
+    def new
+        @kopi = Kopi.new
+    end
+
+  def create
+    @kopi = Kopi.new(kopi_params)
+
+    @kopi.save
+    redirect_to @kopi
+  end
+
+  def show
+    # deal with the case that we are trying to get a ranger for a park that doesn't exist
+
+    @kopi = Kopi.find(params[:id])
+
+    # if params[:origin_id].to_i != @kopi.origin.id
+    #   # do something
+    # end
+  end
+
+private
+
+  def kopi_params
+    params.require(:kopi).permit(:name, :roast)
+  end
+end
---
+++ db/seeds.rb
@@ -0,0 +1,20 @@
+Roast.destroy_all
+
+Roast.create(name: "Cinnamon", roastedness: 1)
+Roast.create(name: "Light", roastedness: 2)
+Roast.create(name: "City/Medium", roastedness: 3)
+Roast.create(name: "Full City", roastedness: 4)
+Roast.create(name: "Dark", roastedness: 5)
+Roast.create(name: "French", roastedness: 6)
+Roast.create(name: "Italian", roastedness: 7)
+
+
+Origin.destroy_all
+
+Origin.create(name: "Farmland", location: "Singapore", phone: "9092 0123")
+Origin.create(name: "Overgrowth Farm", location: "USA", phone: "9810 6692 0123")
+Origin.create(name: "Forested Farm", location: "Malaysia", phone: "6902 1092 0199")
+Origin.create(name: "Cooling Mountain", location: "Canada", phone: "9999 2092 0129")
+Origin.create(name: "High Up in the Sky", location: "Skyland", phone: "9810 0087 0392")
+Origin.create(name: "Hidden Gem", location: "Sri Lanka", phone: "9810 0092 0123")
+Origin.create(name: "Tall Mountain", location: "USA", phone: "9938 0292 1092")
