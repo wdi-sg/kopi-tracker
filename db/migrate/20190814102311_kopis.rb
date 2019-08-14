7f9280dd9be616fda85a2fa469a2914887742d3f
emijay <hilmi.johari@yahoo.com.sg>
2019-08-14 23:05:50 +0800

    first commit/ nested routes set up

---
+++ db/migrate/20190814102311_kopis.rb
@@ -0,0 +1,12 @@
+class Kopis < ActiveRecord::Migration[5.2]
+  def change
+
+    create_table :kopis do |t|
+      t.string :name
+      t.string :roastedness
+      t.references :farm
+      t.timestamps
+    end
+
+  end
+end
