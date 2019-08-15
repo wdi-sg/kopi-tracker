641174fc40c0d7b5fdb5bd0ae62fceb062872f33
kach92 <kenny.ang9206@gmail.com>
2019-08-15 19:41:13 +0800

    done sort kopi by popularity(based on how many customers stocked them) using ruby method. customer sort was done using activerecord query method, not really friendly

--- app/controllers/kopis_controller.rb
+++ app/controllers/kopis_controller.rb
@@ -2,6 +2,15 @@ class KopisController < ApplicationController
     before_action :authenticate_user!, :except => [ :show, :index ]
     def index
         @kopis = Kopi.all
+        if params.has_key?(:sort)
+            if(params[:sort] == "asc" )
+                @kopis = @kopis.sort{|a,b| b.customers.length <=> a.customers.length}
+            else
+                @kopis = @kopis.sort{|a,b| a.customers.length <=> b.customers.length}
+            end
+        else
+            @kopis
+        end
     end
 
     def new
