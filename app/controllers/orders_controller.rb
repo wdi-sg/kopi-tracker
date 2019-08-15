8cc95a6b675ec2e7976306911b1a0274ca3c9248
FrederickKu <timothyfrederick.ku@gmail.com>
2019-08-15 08:41:23 +0800

    added login and some access permission

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,9 +1,19 @@
 class OrdersController < ApplicationController
+  before_action :authenticate_user!
   def index
-    @orders = Order.all
+    if (current_user.admin)
+      @orders = Order.all
+    else
+      @orders = Order.all.where(user_id: current_user.id)
+    end
+    p @orders
   end
 
   def show
+    @order = Order.find(params[:id])
+    if @order.user_id != current_user.id
+      redirect_to orders_path
+    end
   end
 
   def new
@@ -12,6 +22,7 @@ class OrdersController < ApplicationController
 
   def edit
     @order = Order.find(params[:id])
+    @kopis = Kopi.all
   end
 
   def create
@@ -19,21 +30,37 @@ class OrdersController < ApplicationController
 
     @order.total_price = @order.kopi.price * @order.weight
 
+    @order.user_id = current_user.id
+
     @order.save
 
-    redirect_to @orders
+    redirect_to @order
   end
 
   def update
+    @order = Order.find(params[:id])
+
+    kopi_hash = order_params
+
+    kopi = Kopi.find(kopi_hash[:kopi_id])
+
+    kopi_hash["total_price"] = kopi[:price].to_f * kopi_hash[:weight].to_f
+
+    @order.update(kopi_hash)
+
+    redirect_to @order
   end
 
   def destroy
+    @order = Order.find(params[:id])
+
+    @order.destroy
+
+    redirect_to orders_path
   end
 
   private
   def order_params
     params.require(:order).permit(:kopi_id, :weight)
   end
- 
-
 end
--- app/models/order.rb
+++ app/models/order.rb
@@ -1,3 +1,4 @@
 class Order < ActiveRecord::Base
 	belongs_to :kopi
+	belongs_to :user
 end
