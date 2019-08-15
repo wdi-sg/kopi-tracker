51cdc4779f46a54173f3ea51316b93963630d4e2
yoda14 <choobthenoob@hotmail.com>
2019-08-16 02:19:48 +0800

    Planted and watered some erbs

--- jitpuay/app/controllers/farms_controller.rb
+++ jitpuay/app/controllers/farms_controller.rb
@@ -1,14 +1,7 @@
 class FarmsController < ApplicationController
 
   def index
-    # test to see if we are at /parks/:id/rangers or /rangers
-    if params.has_key?(:farm_id)
-      # get all the rangers for a specific park
-      @kopis = Kopi.where(farm_id: params[:farm_id] )
-    else
-      # get all rangers
-      @kopis = Kopi.all
-    end
+    @farms = Farm.all
   end
 
   def new
