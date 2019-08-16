433fc046b51b6217efbe9979195977387f79325f
Nicholas Bingei <nbinged@gmail.com>
2019-08-16 16:03:38 +0800

    added devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
+  devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  root 'origins#index'
+  root 'kopis#index'
   get '/origins' => 'origins#index', as: 'origins'
   get '/origins/new' => 'origins#new', as: 'new_origin'
   post '/origins' => 'origins#create'
