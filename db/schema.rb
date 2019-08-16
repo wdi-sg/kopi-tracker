d8b59413e367b395acbc6b5d0818150a5048eeb4
EliseChia <swedesurrender@gmail.com>
2019-08-16 16:34:40 +0800

    deleted duplicate migration file

--- db/schema.rb
+++ db/schema.rb
@@ -66,7 +66,7 @@ ActiveRecord::Schema.define(version: 2019_08_15_085711) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.boolean "admin"
+    t.boolean "admin", default: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
