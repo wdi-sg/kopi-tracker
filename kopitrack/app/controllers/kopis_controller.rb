aecfaa4a5327022ef341fb92cc29cd744c7719ee
Yew <limtheanyew@gmail.com>
2019-08-16 16:13:30 +0800

    Updated

--- kopitrack/app/controllers/kopis_controller.rb
+++ kopitrack/app/controllers/kopis_controller.rb
@@ -2,4 +2,23 @@ class KopisController < ApplicationController
     def new
         @origins = Origin.all
     end
+    def index
+        @kopis = Kopi.all
+    end
+     def show
+    # deal with the case that we are trying to get a origin for a kopi that doesn't exist
+
+    @kopi = Kopi.find(params[:id])
+  end
+   def edit
+  end
+
+  def create
+  end
+
+  def update
+  end
+
+  def destroy
+  end
 end
