0c80781943d48900f7a92c0d9effefd54854c111
DonC24 <51435215+DonC24@users.noreply.github.com>
2019-08-16 17:08:41 +0800

    Added route and page for new kopi

--- kopi-tracker/app/controllers/kopis_controller.rb
+++ kopi-tracker/app/controllers/kopis_controller.rb
@@ -3,6 +3,11 @@ class KopisController < ApplicationController
     @kopis = Kopi.all
   end
 
+  def new
+    @origins = Origin.all
+    @roasts = Roast.all
+  end
+
   def show
     @kopi = Kopi.find(params[:id])
   end
