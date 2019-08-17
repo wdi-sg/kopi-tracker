c09d3e11c36b68e5599f469ce983e93c9e8d5b79
kinskin <md.asshikin@gmail.com>
2019-08-18 03:23:19 +0800

    implemented user authentication for keith kopi_tracker

--- kopi-tracker/db/schema.rb
+++ kopi-tracker/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_08_14_083125) do
+ActiveRecord::Schema.define(version: 2019_08_17_184330) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,8 @@ ActiveRecord::Schema.define(version: 2019_08_14_083125) do
     t.text "phone"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "user_id"
+    t.index ["user_id"], name: "index_farms_on_user_id"
   end
 
   create_table "kopis", force: :cascade do |t|
@@ -28,14 +30,34 @@ ActiveRecord::Schema.define(version: 2019_08_14_083125) do
     t.bigint "roast_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "user_id"
     t.index ["farm_id"], name: "index_kopis_on_farm_id"
     t.index ["roast_id"], name: "index_kopis_on_roast_id"
+    t.index ["user_id"], name: "index_kopis_on_user_id"
   end
 
   create_table "roasts", force: :cascade do |t|
     t.text "roastedness"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "user_id"
+    t.index ["user_id"], name: "index_roasts_on_user_id"
   end
 
+  create_table "users", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.boolean "admin", default: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  end
+
+  add_foreign_key "farms", "users"
+  add_foreign_key "kopis", "users"
+  add_foreign_key "roasts", "users"
 end
