9cbed76afeff6143621fa9307307b9ec77701c62
kach92 <kenny.ang9206@gmail.com>
2019-08-18 14:49:03 +0800

    added bootstrap to rails and added a simple navbar for the app

--- db/migrate/20190813111638_kopis.rb
+++ db/migrate/20190813111638_kopis.rb
@@ -3,6 +3,7 @@ class Kopis < ActiveRecord::Migration[5.2]
     create_table :kopis do |t|
         t.string :name
         t.string :roast
+        t.text :img_url
         t.references :origin
         t.timestamps
     end
