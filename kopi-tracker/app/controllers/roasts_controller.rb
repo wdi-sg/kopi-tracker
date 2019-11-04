78595e16125c96831540d07e002e0f9a250132cd
Maria Dimitrijevic <maria.dimitrijevikj@gmail.com>
2019-11-05 00:49:33 +0800

    Migrations and CRUD for index, create, show,  kopis, origins and roasts

---
+++ kopi-tracker/app/controllers/roasts_controller.rb
@@ -0,0 +1,41 @@
+class RoastsController < ApplicationController
+    # def index
+    #     @roasts = Roast.all
+    #   end
+    # def show
+    #     @roast = Roast.find(params[:id])
+    #   end
+
+    # def new
+    # end
+
+    # def edit
+    #     @roast = Roast.find(params[:id])
+    #   end
+
+    # def create
+    #     @roast = Roast.new(roast_params)
+
+    #     @roast.save
+    #     redirect_to @roast
+    #   end
+
+    #   def update
+    #     @roast = Roast.find(params[:id])
+
+    #     @roast.update(roast_params)
+    #     redirect_to @roast
+    #   end
+
+    #   def destroy
+    #     @roast = Roast.find(params[:id])
+    #     @roast.destroy
+
+    #     redirect_to root_path
+    #   end
+  end
+
+  # private
+  # def roast_params
+  #   params.require(:roast).permit(:name)
+  # end
---
+++ kopi-tracker/config/routes.rb
@@ -0,0 +1,21 @@
+Rails.application.routes.draw do
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+ root 'origins#index'
+  get '/origins' => 'origins#index', as: 'origins'
+  get '/origins/new' => 'origins#new', as: 'new_origin'
+  post '/origins' => 'origins#create'
+  get '/origins/:id' => 'origins#show' , as: 'origin'
+  get '/origins/:id/edit' => 'origins#edit', as: 'edit_origin'
+  patch '/origins/:id' => 'origins#update'
+  delete '/origins/:id' => 'origins#destroy'
+
+
+  get '/kopis' => 'kopis#index', as: 'kopis'
+  get '/kopis/new' => 'kopis#new', as: 'new_kopi'
+  post '/kopis' => 'kopis#create'
+  get '/kopis/:id' => 'kopis#show' , as: 'kopi'
+  get '/kopis/:id/edit' => 'kopis#edit', as: 'edit_kopi'
+  patch '/kopis/:id' => 'kopis#update'
+  delete '/kopis/:id' => 'kopis#destroy'
+
+end
