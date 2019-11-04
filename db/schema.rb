a1054b7b9a279d98e3fa6e43eaedea9936d5424d
jasminesis <jasmine-aips@hotmail.com>
2019-11-04 18:15:29 +0800

    created roasts table and seeded it

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_04_084549) do
+ActiveRecord::Schema.define(version: 2019_11_04_095919) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -29,6 +29,10 @@ ActiveRecord::Schema.define(version: 2019_11_04_084549) do
     t.integer "phone"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "roasts", force: :cascade do |t|
+    t.text "name"
   end
 
 end
