327e9c9e9a52bda8236c1e090defeb06b7b35b39
David M <davidmosk@gmail.com>
2019-11-04 20:59:03 +0800

    attempt to add associations, but not working...

--- app/controllers/kopis_controller.rb
+++ app/controllers/kopis_controller.rb
@@ -1,2 +1,15 @@
 class KopisController < ApplicationController
+
+    def create
+        @kopi = Kopi.new(kopi_params)
+        @kopi.save
+        redirect_to @kopi
+    end
+
+
+    private 
+    def kopi_params
+        params.require(:kopi).permit(:name, :roast, :origin)
+    end
+
 end
--- app/controllers/roasts_controller.rb
+++ app/controllers/roasts_controller.rb
@@ -1,2 +1,19 @@
 class RoastsController < ApplicationController
+
+    def new
+        if( params[:roast_id].present? )
+          @roast_id = params[:roast_id]
+        end
+        @roasts = Roast.all
+      end
+
+    def show
+        @roast = Roast.find(params[:id])
+      end
+
+    private
+
+    def ranger_params
+        params.require(:roast).permit(:name, :roast_id, :origin_id)
+    end
 end
