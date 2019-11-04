3117f51f9ddc2e8e99c31ea0a32c62a801b15ba6
SYNG <simyenng@gmail.com>
2019-11-04 19:56:17 +0800

    Complete CRUD for origins and kopis

---
+++ app/controllers/kopis_controller.rb
@@ -0,0 +1,51 @@
+class KopisController < ApplicationController
+
+  def index
+    # test to see if we are at /origins/:id/kopis or /kopis
+    if params.has_key?(:origin_id)
+      # get all the kopis for a specific origin
+      @kopis = Kopi.where(origin_id: params[:origin_id] )
+    else
+      # get all kopis
+      @kopis = Kopi.all
+    end
+  end
+
+  def show
+    @kopi = Kopi.find(params[:id])
+  end
+
+  def new
+    @origins = Origin.all
+  end
+
+  def edit
+    @kopi = Kopi.find(params[:id])
+    @origins = Origin.all
+  end
+
+  def create
+    @kopi = Kopi.new(kopi_params)
+    @kopi.save
+    redirect_to @kopi
+  end
+
+  def update
+    @kopi = Kopi.find(params[:id])
+    @kopi.update(kopi_params)
+    redirect_to @kopi
+  end
+
+  def destroy
+    @kopi = Kopi.find(params[:id])
+    @kopi.destroy
+    redirect_to root_path
+  end
+
+private
+
+  def kopi_params
+    params.require(:kopi).permit(:name, :roast, :origin_id)
+  end
+
+end
