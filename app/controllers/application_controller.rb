23979d446ccbce45c0145b5f53c044fa2cb6588b
dannyozh <danielongzhihao@gmail.com>
2019-11-05 21:17:38 +0800

    added bootstrap and two navbars, depending on user login status

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,6 @@ class ApplicationController < ActionController::Base
 
   # Overwriting the sign_out redirect path method
   def after_sign_out_path_for(resource_or_scope)
-    user_session_path
+    root_path
   end
 end
