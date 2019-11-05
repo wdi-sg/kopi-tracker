7954d7e2fd983b23c2779176dcc129c6ce787e0b
dannyozh <danielongzhihao@gmail.com>
2019-11-05 12:54:06 +0800

    relational tables up and running. finally!!!

--- app/controllers/kopis_controller.rb
+++ app/controllers/kopis_controller.rb
@@ -1,15 +1,37 @@
 class KopisController < ApplicationController
-    def index
-      if params.has_key?(:origin_id)
-        # get all the rangers for a specific park
-        @kopis = Kopi.where(origin_id: params[:origin_id])
-      else
-        # get all rangers
-        @kopis = Kopi.all
-      end
-    end
-  
-    def show
-        @kopi = Kopi.find_by(id: params[:id] )
-    end
+  def index
+    @kopis = Kopi.all
   end
+
+  def create
+    @kopi = Kopi.new(kopi_params)
+    @kopi.save
+    redirect_to @kopi
+  end
+
+  def show
+    @kopi = Kopi.find(params[:id])
+  end
+
+  def edit
+    @kopi = Kopi.find(params[:id])
+  end
+
+  def update
+    @kopi = Kopi.find(params[:id])
+    @kopi.update(kopi_params)
+    redirect_to @kopi
+  end
+
+  def destroy
+    @kopi = Kopi.find(params[:id])
+    @kopi.destroy
+    redirect_to @kopi
+  end
+
+  private
+
+  def kopi_params
+    params.require(:kopi).permit(:name, :roast, :origin_id)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root "coffees#index"
+  root "kopis#index"
 
-  resources :coffees
+  resources :kopis
+  resources :origins
 end
