bd5e71801adc129a2b8df31e12c7227b45defa39
TanWeeKiat <tanwee.kiat@live.com.sg>
2019-11-05 09:02:52 +0800

    done with deliverables

---
+++ app/controllers/kopis_controller.rb
@@ -0,0 +1,60 @@
+class KopisController < ApplicationController
+
+    def index
+        # test to see if we are at /parks/:id/rangers or /rangers
+        if params.has_key?(:origin_id)
+          # get all the rangers for a specific park
+          @kopis = Kopi.where(origin_id: params[:origin_id] )
+        else
+          # get all rangers
+          @kopis = Kopi.all
+          @origins = Origin.all
+        end
+      end
+
+    def new
+      @origins = Origin.all
+    end
+
+    def edit
+        @kopi = Kopi.find(params[:id])
+    end
+  
+    def create
+      @kopi = Kopi.new(kopi_params)
+  
+      @kopi.save
+      redirect_to @kopi
+    end
+  
+    def show
+      # deal with the case that we are trying to get a ranger for a park that doesn't exist
+  
+      @kopi = Kopi.find(params[:id])
+  
+      if params[:origin_id].to_i != @kopi.origin.id
+        # do something
+      end
+    end
+
+    def update
+        @kopi = Kopi.find(params[:id])
+
+        @kopi.update(kopi_params)
+        redirect_to @kopi
+    end
+
+    def destroy
+        @kopi = Kopi.find(params[:id])
+        @kopi.destroy
+  
+        redirect_to root_path
+    end
+  
+  
+  private
+  
+    def kopi_params
+      params.require(:kopi).permit(:name, :origin_id, :roast)
+    end
+  end
