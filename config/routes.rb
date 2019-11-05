aa5137ecbbe5903e3e9f9a01d61063f68dc34c73
syahirah <syahirah.suradi@gmail.com>
2019-11-05 17:10:42 +0800

    added log in/log out and authentication

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,8 @@
 Rails.application.routes.draw do
+  devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  root 'farms#index'
-  get '/farms' => 'farms#index', as: 'farms'
-  get '/farms/new' => 'farms#new', as: 'new_farm'
-  post '/farms' => 'farms#create'
-  get '/farms/:id' => 'farms#show' , as: 'farm'
-  get '/farms/:id/edit' => 'farms#edit', as: 'edit_farm'
-  patch '/farms/:id' => 'farms#update'
-  delete '/farms/:id' => 'farms#destroy'
-
+  root 'kopis#index'
   get '/kopis' => 'kopis#index', as: 'kopis'
   get '/kopis/new' => 'kopis#new', as: 'new_kopi'
   post '/kopis' => 'kopis#create'
@@ -17,6 +10,15 @@ Rails.application.routes.draw do
   get '/kopis/:id/edit' => 'kopis#edit', as: 'edit_kopi'
   patch '/kopis/:id' => 'kopis#update'
   delete '/kopis/:id' => 'kopis#destroy'
+
+  #root 'farms#index'
+  get '/farms' => 'farms#index', as: 'farms'
+  get '/farms/new' => 'farms#new', as: 'new_farm'
+  post '/farms' => 'farms#create'
+  get '/farms/:id' => 'farms#show' , as: 'farm'
+  get '/farms/:id/edit' => 'farms#edit', as: 'edit_farm'
+  patch '/farms/:id' => 'farms#update'
+  delete '/farms/:id' => 'farms#destroy'
 
   get '/farms/:farm_id/kopis' => 'kopis#index', as: 'farm_kopis'
   get '/farms/:farm_id/kopis/new' => 'kopis#create', as: 'new_farm_kopi'
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_05_074259) do
+ActiveRecord::Schema.define(version: 2019_11_05_090018) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -25,12 +25,27 @@ ActiveRecord::Schema.define(version: 2019_11_05_074259) do
     t.string "name"
     t.bigint "roast_id"
     t.bigint "farm_id"
+    t.bigint "user_id"
     t.index ["farm_id"], name: "index_kopis_on_farm_id"
     t.index ["roast_id"], name: "index_kopis_on_roast_id"
+    t.index ["user_id"], name: "index_kopis_on_user_id"
   end
 
   create_table "roasts", force: :cascade do |t|
     t.string "name"
   end
 
+  create_table "users", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  end
+
+  add_foreign_key "kopis", "users"
 end
