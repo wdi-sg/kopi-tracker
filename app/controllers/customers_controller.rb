4ee4f1f8904c185a972700ee38a6444911da1e96
Nathanael Tan <nathanael.tan92@gmail.com>
2019-11-06 17:33:53 +0800

    can add customer and kopis shows customers that carry it

---
+++ app/controllers/customers_controller.rb
@@ -0,0 +1,28 @@
+class CustomersController < ApplicationController
+    def new
+        @kopis = Kopi.all
+    end
+    def index
+        @customers = Customer.all
+    end
+
+    def create
+        @customer = Customer.new(customer_params)
+        @customer.save
+      
+       redirect_to @customer
+    end
+
+    def show
+        @customer = Customer.find(params[:id])
+    end
+
+
+    private
+
+    def customer_params
+
+        params.require(:customer).permit(:name, :kopi_ids => [])
+
+    end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -24,5 +24,10 @@ Rails.application.routes.draw do
   patch '/roasts/:id' => 'roasts#update'
   delete '/roasts/:id' => 'roasts#destroy'
 
+  get 'customers' => 'customers#index', as: 'customers'
+  get 'customers/new' => 'customers#new', as: 'new_customer'
+  post 'customers' => 'customers#create'
+  get 'customers/:id' => 'customers#show', as: 'customer'
+
 end
  
