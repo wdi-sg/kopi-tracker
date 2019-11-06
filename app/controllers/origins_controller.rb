04562f7a075f675815d2f8e95ab06af32998e92e
Sharon <sharxnrxse1312@gmail.com>
2019-11-06 09:52:53 +0800

    Part 2 with some bugs

--- app/controllers/origins_controller.rb
+++ app/controllers/origins_controller.rb
@@ -1,26 +1,38 @@
 class OriginsController < ApplicationController
 
   def index
-    @origins = Origin.all
+    @origins = Origin.all.order("location ASC")
   end
 
   def new
   end
 
-  def show
+  def edit
     @origin = Origin.find(params[:id])
-    @kopis = Kopi.where(origin_id: params[:id])
-    # render plain: @origin
   end
 
-  def create
+def create
     @origin = Origin.new(origin_params)
     @origin.save
+    redirect_to @origin
+  end
+
+  def update
+    @origin = Origin.find(params[:id])
+    @origin.update(origin_params)
+    redirect_to @origin
+  end
+
+  def destroy
+    @origin = Origin.find(params[:id])
+    @origin.destroy
     redirect_to root_path
   end
 
-  private
-    def origin_params
-      params.require(:origin).permit(:location, :phone)
-    end
+private
+
+  def origin_params
+    params.require(:origin).permit(:location, :phone)
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,15 @@
 Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'origins#index'
+  root 'kopis#index'
+  get '/kopis' => 'kopis#index', as: 'kopis'
+  get '/kopis/new' => 'kopis#new', as: 'new_kopi'
+  post '/kopis' => 'kopis#create'
+  get '/kopis/:id' => 'kopis#show' , as: 'kopi'
+  get '/kopis/:id/edit' => 'kopis#edit', as: 'edit_kopi'
+  patch '/kopis/:id' => 'kopis#update'
+  delete '/kopis/:id' => 'kopis#destroy'
+
   get '/origins' => 'origins#index', as: 'origins'
   get '/origins/new' => 'origins#new', as: 'new_origin'
   post '/origins' => 'origins#create'
@@ -10,5 +18,9 @@ Rails.application.routes.draw do
   patch '/origins/:id' => 'origins#update'
   delete '/origins/:id' => 'origins#destroy'
 
-  get '/origins/:id/new' => 'kopis#new', as: 'new_kopi'
+  get '/origins/:origin_id/kopis' => 'kopis#index', as: 'origin_kopis'
+  get '/origins/:origin_id/kopis/new' => 'kopis#new', as: 'new_origin_kopi'
+  post '/origins/:origin_id/kopis' => 'kopis#create'
+
+  get '/roasts/:roast_id/kopis' => 'kopis#index', as: 'roast_kopis'
 end
