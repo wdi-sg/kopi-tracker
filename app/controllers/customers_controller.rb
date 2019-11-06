ecdc1e150487568a0ba1a3318d4112a2ee1726cc
datguyrhy <datguyrhy@Branz.localdomain>
2019-11-07 02:39:57 +0800

    added customer table and controllers

---
+++ app/controllers/customers_controller.rb
@@ -0,0 +1,55 @@
+class CustomersController < ApplicationController
+  before_action :set_customer, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @customers = Customer.all
+  end
+
+  def show
+    @customer = Customer.find(params[:id])
+
+  end
+
+  def new
+    @customer = Customer.new
+    @kopis = Kopi.all
+  end
+
+  def edit
+    @customer = Customer.find(params[:id])
+    @kopis = Kopi.all
+  end
+
+  def create
+    @customer = Customer.new(customer_params)
+    @customer.save
+
+  end
+
+
+  def update
+    @customer = Customer.find(params[:id])
+
+    @customer.update(customer_params)
+
+    redirect_to @customer
+
+
+  end
+
+
+  def destroy
+
+  end
+
+  private
+
+  def set_customer
+    @customer = Customer.find(params[:id])
+  end
+
+
+  def customer_params
+    params.require(:customer).permit(:name, :kopi_ids => [])
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
-
+  resources :customers
   root "kopis#index"
   resources :kopis
   resources :origins
