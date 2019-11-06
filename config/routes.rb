6a011b2d9545b6431c6037fd16b9f334ae531f0f
SYNG <simyenng@gmail.com>
2019-11-06 15:14:33 +0800

    Add model for customers

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
+  resources :roasts
+  resources :customers
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :roasts
 
   root 'kopis#index'
   get '/kopis' => 'kopis#index', as: 'kopis'
