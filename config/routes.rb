9ebda19b7c773fcdcc0989977e6414fa0dd55a77
jasminesis <jasmine-aips@hotmail.com>
2019-11-06 21:50:52 +0800

    fixed database and schema

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :customers
   devise_for :users
   
   resources :kopis
--- db/schema.rb
+++ db/schema.rb
@@ -16,9 +16,15 @@ ActiveRecord::Schema.define(version: 2019_11_06_082154) do
   enable_extension "plpgsql"
 
   create_table "customers", force: :cascade do |t|
-    t.string "name"
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_customers_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_customers_on_reset_password_token", unique: true
   end
 
   create_table "customers_kopis", force: :cascade do |t|
