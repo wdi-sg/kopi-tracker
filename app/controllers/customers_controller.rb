c61d07987fdf85fad4fa3572e2ab5a2600174c8f
tansiewling-hotmail <prudence_tan@hotmail.com>
2019-11-06 17:18:01 +0800

    Created new customer controller file with new method to get all kopis

---
+++ app/controllers/customers_controller.rb
@@ -0,0 +1,24 @@
+class CustomersController < ApplicationController
+
+    def index
+    end
+
+    def new
+        @kopis = Kopi.all
+    end
+
+    def create
+    end
+
+    def show
+    end
+
+    def edit
+    end
+
+    def update
+    end
+
+    def destroy
+    end
+end
