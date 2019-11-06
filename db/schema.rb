178a5983618709d81c566b60fba87da4695f9faa
wongyijin8183 <wongyijin8183@gmail.com>
2019-11-07 02:49:05 +0800

    installed bundle with devise. settings laid out as per gitbook. stuck at "undefined method" at config/routes.rb at "devise_for :users"

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_06_175734) do
+ActiveRecord::Schema.define(version: 2019_11_06_183712) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -32,7 +32,9 @@ ActiveRecord::Schema.define(version: 2019_11_06_175734) do
     t.string "name"
     t.text "roast"
     t.bigint "origin_id"
+    t.bigint "user_id"
     t.index ["origin_id"], name: "index_kopis_on_origin_id"
+    t.index ["user_id"], name: "index_kopis_on_user_id"
   end
 
   create_table "origins", force: :cascade do |t|
@@ -46,4 +48,17 @@ ActiveRecord::Schema.define(version: 2019_11_06_175734) do
     t.text "email"
   end
 
+  create_table "users", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  end
+
+  add_foreign_key "kopis", "users"
 end
