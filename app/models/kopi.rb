f06f1a79730095b8be48d7ecb149cdb08d1ba3b8
reshinto <terencekong2002@gmail.com>
2019-11-06 21:13:43 +0800

    Add customers

--- app/models/kopi.rb
+++ app/models/kopi.rb
@@ -2,5 +2,6 @@ class Kopi < ApplicationRecord
   belongs_to :origin
   belongs_to :roast
   belongs_to :user
+  has_and_belongs_to_many :customers
   validates :name, presence: true
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,5 @@ Rails.application.routes.draw do
   resources :origins
   resources :roasts
   resources :kopis
+  resources :customers
 end
