da57e6826cd6fa5ecd0123270ea32632c0d88653
edenlim <edenlim123@gmail.com>
2019-11-06 09:03:34 +0800

    Added login screen; Next update: Admin

--- app/controllers/kopis_controller.rb
+++ app/controllers/kopis_controller.rb
@@ -1,5 +1,5 @@
 class KopisController < ApplicationController
-
+  before_action :authenticate_user!, :except => [ :show, :index ]
   def index
     @kopis = Kopi.all
   end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_04_085145) do
+ActiveRecord::Schema.define(version: 2019_11_05_074244) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,7 +21,9 @@ ActiveRecord::Schema.define(version: 2019_11_04_085145) do
     t.bigint "origin_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "user_id"
     t.index ["origin_id"], name: "index_kopis_on_origin_id"
+    t.index ["user_id"], name: "index_kopis_on_user_id"
   end
 
   create_table "origins", force: :cascade do |t|
@@ -31,4 +33,17 @@ ActiveRecord::Schema.define(version: 2019_11_04_085145) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "users", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  end
+
+  add_foreign_key "kopis", "users"
 end
