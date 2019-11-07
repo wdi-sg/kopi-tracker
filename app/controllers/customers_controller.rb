eeffc770fb74c9739a5c2d5f547e88e32db8e0ed
kevinngth <kevin.ng.th@gmail.com>
2019-11-07 09:13:11 +0800

    added login to customers

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -1,4 +1,7 @@
 class CustomersController < ApplicationController
+
+  before_action :authenticate_customer!
+
   def index
     @customers = Customer.all
   end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :customers
   devise_for :users
   root 'kopis#home'
   resources :roasts
