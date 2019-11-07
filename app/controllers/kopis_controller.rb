11c3fc2d6542808a6b3db131780bf7808aed3b34
“kellylimmm” <kellylim.06@gmail.com>
2019-11-07 17:16:18 +0800

    Added Show/Edit/Destroy and formatted it into a table

--- app/controllers/kopis_controller.rb
+++ app/controllers/kopis_controller.rb
@@ -1,3 +1,5 @@
+# DAVID
+
 class KopisController < ApplicationController
     before_action :authenticate_user!, :except => [ :show, :index ]
 
@@ -8,16 +10,32 @@ class KopisController < ApplicationController
 
     end
 
+    def edit
+    @kopi = Kopi.find(params[:id])
+    @roasts = Roast.all
+    @origins = Origin.all
+    p @origins
+
+  end
+
+    def update
+    @kopi = Kopi.find(params[:id])
+    @kopi.update(kopi_params)
+
+    redirect_to @kopi
+
+  end
+
     def create
         @kopi = Kopi.new(kopi_params)
         @kopi.user = current_user
         if @kopi.save
             redirect_to @kopi
-        else 
-            
+        else
+
         @roasts = Roast.all
         @origins = Origin.all
-            render 'new'  
+            render 'new'
          end
     end
 
@@ -27,14 +45,14 @@ class KopisController < ApplicationController
         @origins = Origin.all
 
     end
-    
+
     def show
         @kopi = Kopi.find(params[:id])
 
     end
 
 
-    private 
+    private
     def kopi_params
         params.require(:kopi).permit(:name, :roast_id, :origin_id)
     end
--- app/controllers/origins_controller.rb
+++ app/controllers/origins_controller.rb
@@ -1,7 +1,7 @@
 class OriginsController < ApplicationController
 
     before_action :authenticate_user!, :except => [ :show, :index ]
-    
+
     def index
         @origin = Origin.all
     end
@@ -17,17 +17,38 @@ class OriginsController < ApplicationController
         @origin = Origin.find(params[:id])
     end
 
+    def edit
+          @origin = Origin.find(params[:id])
+        end
+
+    def update
+
+        @origin = Origin.find(params[:id])
+        @origin.update(origin_params)
+
+        redirect_to @origin
+
+    end
+
+    def destroy
+    @origin = Origin.find(params[:id])
+    @origin.destroy
+
+    redirect_to @origin
+  end
+
+
     def create
         @origin = Origin.new(origin_params)
         if @origin.save
             redirect_to @origin
-        else 
-            render 'new'  
+        else
+            render 'new'
          end
 
     end
 
-    private 
+    private
     def origin_params
         params.require(:origin).permit(:location, :phone)
     end
