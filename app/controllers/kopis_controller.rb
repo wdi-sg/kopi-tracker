d7ad5af87bccdf376cf0e094b7daedcbb23049a2
Stuart Myers <stuart.myers@gmail.com>
2020-02-11 20:51:15 +0800

    Can add new Kopis to Database

--- app/controllers/kopis_controller.rb
+++ app/controllers/kopis_controller.rb
@@ -5,15 +5,22 @@ class KopisController < ApplicationController
   end
 
   def show
+    @kopi = Kopi.find(params[:id])
   end
 
   def new
+    @roasts = Roast.all
+    @origins = Origin.all
   end
 
   def edit
   end
 
   def create
+    @kopi = Kopi.new(kopi_params)
+
+    @kopi.save
+    redirect_to @kopi
   end
 
   def update
@@ -22,4 +29,9 @@ class KopisController < ApplicationController
   def destroy
   end
 
+private
+
+  def kopi_params
+    params.require(:kopi).permit(:name, :origin_id, :roast_id)
+  end
 end
