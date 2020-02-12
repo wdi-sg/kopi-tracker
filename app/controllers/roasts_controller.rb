20999fa497636fcf6b568bcf02beb3c18acee31d
Stuart Myers <stuart.myers@gmail.com>
2020-02-12 16:49:23 +0800

    Can edit roasts if admin

--- app/controllers/roasts_controller.rb
+++ app/controllers/roasts_controller.rb
@@ -10,7 +10,22 @@ class RoastsController < ApplicationController
     @kopis = Kopi.all.where(roast: @roast)
   end
 
+  def update
+    @roast = Roast.find(params[:id])
+
+    @roast.update(roast_params)
+    redirect_to @roast
+  end
+
+  def edit
+    @roast = Roast.find(params[:id])
+  end
+
 private
+
+  def roast_params
+    params.require(:roast).permit(:name)
+  end
 
   def check_if_admin
     if :authenticate_user!
