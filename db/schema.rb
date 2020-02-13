9286f967881522bc02067657bf0d8374cc213cf6
joyce-paul <joyce_paul@hotmail.com>
2020-02-13 15:27:09 +0800

    Created "customers_kopis" table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_12_083006) do
+ActiveRecord::Schema.define(version: 2020_02_13_071007) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -28,6 +28,21 @@ ActiveRecord::Schema.define(version: 2020_02_12_083006) do
     t.datetime "locked_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "customers", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "customers_kopis", force: :cascade do |t|
+    t.bigint "customer_id"
+    t.bigint "kopi_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["customer_id"], name: "index_customers_kopis_on_customer_id"
+    t.index ["kopi_id"], name: "index_customers_kopis_on_kopi_id"
   end
 
   create_table "kopis", force: :cascade do |t|
