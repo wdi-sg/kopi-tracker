e28707ad34506ffa51d080406030bef7c2196ce8
Zachariah Chow <zachariahchow@gmail.com>
2020-05-11 19:01:07 +0800

    ran Roast migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_11_104322) do
+ActiveRecord::Schema.define(version: 2020_05_11_105859) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -36,6 +36,12 @@ ActiveRecord::Schema.define(version: 2020_05_11_104322) do
   create_table "origins", force: :cascade do |t|
     t.string "location", null: false
     t.string "phone"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "roasts", force: :cascade do |t|
+    t.string "name"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
