f5296f9bc1ba6e0848a35fc4bb9e8659e16a2620
kokchuantan <kokchuan1996@gmail.com>
2020-05-11 23:14:49 +0800

    added routes and index page for farms

---
+++ app/controllers/farms_controller.rb
@@ -0,0 +1,24 @@
+class FarmsController < ApplicationController
+    def index
+        @farms = Farm.all
+    end
+
+    def  new
+    end
+
+    def create
+    end
+
+    def show
+    end
+
+    def edit
+    end
+
+    def update
+    end
+    
+    def destroy
+    end
+
+end
