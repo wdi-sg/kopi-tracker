6853e8fba21b3f3d5b0d29f682303b2e925a3129
kokchuantan <kokchuan1996@gmail.com>
2020-05-11 23:51:15 +0800

    added function to show/edit/destroy kopi farms

--- app/controllers/farms_controller.rb
+++ app/controllers/farms_controller.rb
@@ -7,18 +7,34 @@ class FarmsController < ApplicationController
     end
 
     def create
+        @farm = Farm.new(farm_params)
+        @farm.save
+        redirect_to @farm
     end
 
     def show
+        @farm = Farm.find(params[:id])
     end
 
     def edit
+        @farm = Farm.find(params[:id])
     end
 
     def update
+        @farm = Farm.find(params[:id])
+        @farm.update(farm_params)
+        redirect_to @farm
     end
     
     def destroy
+        @farm = Farm.find(params[:id])
+        @farm.destroy
+        redirect_to root_path
     end
+    private
 
+  def farm_params
+    params.require(:farm).permit(:name, :location, :phone)
+  end
+  
 end
