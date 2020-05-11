87851ce5e06c36aa5dcd09d9e8c14ba75fd2d412
travisenquiry <travisenquiry@gmail.com>
2020-05-11 21:05:04 +0800

    completed MVP

---
+++ app/controllers/kopis_controller.rb
@@ -0,0 +1,30 @@
+class KopisController < ApplicationController
+
+  def new
+    @kopis = Kopi.all
+    @origins = Origin.all
+  end
+
+  def create
+    @kopi = Kopi.new(kopi_params)
+
+    @kopi.save
+    redirect_to @kopi
+  end
+
+  def show
+    # deal with the case that we are trying to get a ranger for a park that doesn't exist
+
+    @kopi = Kopi.find(params[:id])
+
+    if params[:origin_id].to_i != @kopi.origin.id
+      # do something
+    end
+  end
+
+private
+
+  def kopi_params
+    params.require(:kopi).permit(:name, :roast, :origin_id)
+  end
+end
