1a6e6cce52f42dd1518aee864a37664c178c4532
Zachariah Chow <zachariahchow@gmail.com>
2020-05-12 00:33:56 +0800

    added sales total/kopi/origin to report controller/view

--- app/controllers/report_controller.rb
+++ app/controllers/report_controller.rb
@@ -1,11 +1,26 @@
 class ReportController < ApplicationController
   def index
-    @orders = Order.select("*").joins(:kopi).where(created_at: Date.today.all_day)
+    # @orders = Order.select("*").joins(:kopi).where(created_at: Date.today.all_day)
+
+    @orders = Order.select("*").joins(:kopi)
 
     @total_sales = @orders.reduce(0) {|sum, order| sum + order.weight * order.price_per_pound}
+
+
+    @total_sales_by_kopi = Kopi.all.map do |kopi|
+      {name: kopi[:name], total_sales: @orders.filter{|order| order.kopi_id == kopi.id}.reduce(0) {|sum, order| sum + order.weight * order.price_per_pound}}
+      end
+
+      @orders_by_origin = Kopi.select("*").joins(:orders, :origin)
+
+      @total_sales_by_origin = Origin.all.map do |origin|
+        {location: origin[:location], total_sales: @orders_by_origin.filter{|order| order.origin_id == origin.id}.reduce(0) {|sum, order| sum + order.weight * order.price_per_pound}}
+        end
+
+    end
+
 
     # @sales = @orders.each do |order|
     #   order[:weight] *
     # end
-  end
 end
