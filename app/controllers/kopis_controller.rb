1907f13f11815cc0943dfb90627038a7f57dbcb3
kwansing14 <kwansing@hotmail.sg>
2020-05-12 02:31:53 +0800

    added show edit destroy options for each coffee listed

--- app/controllers/kopis_controller.rb
+++ app/controllers/kopis_controller.rb
@@ -16,13 +16,28 @@ class KopisController < ApplicationController
   end
 
   def edit
-    @article = Kopi.find(params[:id])
+    @kopis = Kopi.find(params[:id])
+    @roasts = Roast.all
+    @origins = Origin.all
+  end
+
+  def update
+    @kopis = Kopi.find(params[:id])
+    @kopis.update(kopi_params)
+    redirect_to @kopis
   end
 
   def show
     @kopis = Kopi.find(params[:id])
     @roasts = Roast.find(@kopis.roast_id)
     @origins = Origin.find(@kopis.origin_id)
+  end
+
+    def destroy
+    @kopis = Kopi.find(params[:id])
+    @kopis.destroy
+
+    redirect_to root_path
   end
 
 end
