e315ffde48b5763205be3928ee2544713d146a66
chelsejw <chelsejw@gmail.com>
2020-05-12 02:35:00 +0800

    added show routes for kopis

--- app/models/kopi_farm.rb
+++ app/models/kopi_farm.rb
@@ -1,2 +1,3 @@
 class KopiFarm < ActiveRecord::Base
+      has_many :kopi
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  
+  get '/kopi/:id' => 'kopis#show', as: 'kopi'
   get '/kopi_farms/new' => 'kopi_farms#new', as: 'new_kopi_farm'
   get '/kopi_farms/:id/edit' => 'kopi_farms#edit', as: 'edit_kopi_farm'
   get '/kopi_farms/:id' => 'kopi_farms#show', as: 'kopi_farm'
