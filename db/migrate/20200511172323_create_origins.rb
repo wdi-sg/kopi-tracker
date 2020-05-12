b0e02557c5e44cbfe84c1fdb96223f70a5d71c20
Kenan <xinkenan.11@hotmail.com>
2020-05-12 13:02:56 +0800

    add migration and models

---
+++ db/migrate/20200511172323_create_origins.rb
@@ -0,0 +1,10 @@
+class CreateOrigins < ActiveRecord::Migration[6.0]
+  def change
+    create_table :origins do |t|
+      t.string :location
+      t.string :phone
+
+      t.timestamps
+    end
+  end
+end
