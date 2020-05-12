51c5272443d3a4980b8422e7e8993a7978b38fc1
osbornechan <osbornecab@gmail.com>
2020-05-13 00:33:51 +0800

    Restored new, create, show routes

--- kopi_tracker/app/controllers/kopis_controller.rb
+++ kopi_tracker/app/controllers/kopis_controller.rb
@@ -7,8 +7,16 @@ class KopisController < ApplicationController
   end
 
   def create
+    @kopi = Kopi.create(kopi_params)
+    redirect_to @kopi
   end
 
   def show
+    @kopi = Kopi.find(params[:id])
   end
+
+  private
+    def kopi_params
+      params.require(:kopi).permit(:name, :roast_id, :origin_id, :price)
+    end
 end
--- kopi_tracker/config/routes.rb
+++ kopi_tracker/config/routes.rb
@@ -1,14 +1,4 @@
 Rails.application.routes.draw do
-  get 'kopis/index'
-  get 'kopis/new'
-  get 'kopis/create'
-  get 'kopis/show'
-  get 'index/new'
-  get 'index/create'
-  get 'index/show'
-  get 'kopis/new'
-  get 'kopis/create'
-  get 'kopis/show'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :kopis
 end
