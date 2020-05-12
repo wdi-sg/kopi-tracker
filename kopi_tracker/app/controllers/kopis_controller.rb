5887099473d59e63cb90641949dd2a70c6c6cc77
osbornechan <osbornecab@gmail.com>
2020-05-13 03:14:10 +0800

    Added edit, update, destroy controllers to kopi

--- kopi_tracker/app/controllers/kopis_controller.rb
+++ kopi_tracker/app/controllers/kopis_controller.rb
@@ -17,6 +17,24 @@ class KopisController < ApplicationController
     @kopi = Kopi.find(params[:id])
   end
 
+  def edit
+    @kopi = Kopi.find(params[:id])
+  end
+
+  def update
+    @kopi = Kopi.find(params[:id])
+    @kopi.update(kopi_params)
+
+    redirect_to @kopi
+  end
+
+  def destroy
+    @kopi = Kopi.find(params[:id])
+    @kopi.destroy
+
+    redirect_to root_path
+  end
+
   private
     def kopi_params
       params.require(:kopi).permit(:name, :roast_id, :origin_id, :price)
