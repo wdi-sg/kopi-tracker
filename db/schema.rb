90cabd26cdcd6f0a81ebccc65697da5f60deaa81
chelsejw <chelsejw@gmail.com>
2020-05-12 23:24:42 +0800

    add devise starter code

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_12_151504) do
+ActiveRecord::Schema.define(version: 2020_05_12_152151) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -24,12 +24,27 @@ ActiveRecord::Schema.define(version: 2020_05_12_151504) do
     t.string "name"
     t.bigint "roast_id"
     t.bigint "kopi_farm_id"
+    t.bigint "user_id"
     t.index ["kopi_farm_id"], name: "index_kopis_on_kopi_farm_id"
     t.index ["roast_id"], name: "index_kopis_on_roast_id"
+    t.index ["user_id"], name: "index_kopis_on_user_id"
   end
 
   create_table "roasts", force: :cascade do |t|
     t.string "name"
   end
 
+  create_table "users", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  end
+
+  add_foreign_key "kopis", "users"
 end
