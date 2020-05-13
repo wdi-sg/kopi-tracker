261eb36604be1a6c1336e56d33d30d3b64580ab9
arindamc14 <arindamchowdhury91@gmail.com>
2020-05-13 10:25:22 +0800

    fixed the user migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_12_022525) do
+ActiveRecord::Schema.define(version: 2020_05_13_013756) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "kopi", force: :cascade do |t|
     t.string "name"
+    t.bigint "user_id"
+    t.index ["user_id"], name: "index_kopi_on_user_id"
   end
 
   create_table "origin", force: :cascade do |t|
@@ -32,4 +34,17 @@ ActiveRecord::Schema.define(version: 2020_05_12_022525) do
     t.index ["kopi_id"], name: "index_roast_on_kopi_id"
   end
 
+  create_table "users", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  end
+
+  add_foreign_key "kopi", "users"
 end
