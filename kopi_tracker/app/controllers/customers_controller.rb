7ab2cc67c820ef544a8d67b923caa1a89ad58e69
osbornechan <osbornecab@gmail.com>
2020-05-14 12:30:11 +0800

    Added edit/ destroy for customers

--- kopi_tracker/app/controllers/customers_controller.rb
+++ kopi_tracker/app/controllers/customers_controller.rb
@@ -4,13 +4,44 @@ class CustomersController < ApplicationController
   end
 
   def new
+    @kopis = Kopi.all
   end
 
-  def edit
+  def create
+    @customer = Customer.new(customer_params)
+
+    if @customer.save
+      redirect_to @customer
+    else
+      render 'new'
+    end
   end
 
   def show
     @customer = Customer.find(params[:id])
+  end
+
+  def edit
+    @customer = Customer.find(params[:id])
+    @kopis = Kopi.all
+  end
+
+  def update
+    @customer = Customer.find(params[:id])
+    update_happened = @customer.update(customer_params)
+
+    if update_happened
+      redirect_to @customer
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @customer = Customer.find(params[:id])
+    @customer.destroy
+
+    redirect_to customers_path
   end
 
   private
